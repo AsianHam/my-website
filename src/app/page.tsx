eea49c52e36b7d8c015e92fc59aaa97de97113cd
Abraham Oh <abeoh1@gmail.com>
2024-05-20 20:00:36 -0400

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-import { Caret } from "@/components/caret";
 import { DeveloperTyping } from "@/components/developer-typing";
 
 export default function Home() {
