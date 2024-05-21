cf8530e46bcfda6bd305ce54fe9e22c0ab828a63
Abraham Oh <abeoh1@gmail.com>
2024-05-20 21:42:37 -0400

    add more words to the typing animation

--- src/components/developer-typing.tsx
+++ src/components/developer-typing.tsx
@@ -1,22 +1,47 @@
-"use client";
+'use client';
 
-import { Caret } from "@/components/caret";
-import { animate, motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect } from "react";
+import { Caret } from '@/components/caret';
+import { animate, motion, useMotionValue, useTransform } from 'framer-motion';
+import { useEffect } from 'react';
 
 export const DeveloperTyping = () => {
-  const text = "Full-Stack Developer";
+  const text = [
+    'Full-Stack Developer',
+    'Software Engineer',
+    'Front-End Architect',
+    'UX Enthusiast',
+    'Professional Programmer',
+  ];
+  const textIndex = useMotionValue(0);
   const count = useMotionValue(0);
   const rounded = useTransform(count, (latest) => Math.round(latest));
-  const displayText = useTransform(rounded, (latest) => text.slice(0, latest));
+  const selectedText = useTransform(textIndex, (latest) => text[latest] || '');
+  const displayText = useTransform(rounded, (latest) =>
+    selectedText.get().slice(0, latest)
+  );
+  const updated = useMotionValue(true);
 
   useEffect(() => {
-    const controls = animate(count, text.length, {
-      type: "tween",
+    animate(count, 60, {
+      type: 'tween',
       duration: 1.5,
-      ease: "easeInOut",
+      ease: 'easeIn',
+      repeat: Infinity,
+      repeatType: 'reverse',
+      repeatDelay: 1,
+      onUpdate(latest) {
+        if (updated.get() === true && latest > 0) {
+          updated.set(false);
+        } else if (updated.get() === false && latest === 0) {
+          if (textIndex.get() === text.length - 1) {
+            textIndex.set(0);
+          } else {
+            textIndex.set(textIndex.get() + 1);
+          }
+          updated.set(true);
+        }
+      },
     });
-    return controls.stop;
   });
 
   return (
