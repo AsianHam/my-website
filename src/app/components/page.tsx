0829764e0c708a4c147aac0651e41e54e110443a
Abraham Oh <abeoh1@gmail.com>
2024-05-27 17:24:27 -0400

    add the animation section to the components page

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { ColorDisplay } from '@/components/color-display';
 import { HorizontalNavBar } from '@/components/horizontal-nav-bar';
 import { MobileNavBar } from '@/components/mobile-nav-bar';
+import { AnimationSection } from '@/components/sections/animation-section';
+import { ColorSection } from '@/components/sections/color-section';
 import { useRef } from 'react';
 
 const ComponentsPage = () => {
@@ -34,53 +35,15 @@ const ComponentsPage = () => {
             </h2>
           </div>
         </section>
-        <section className='w-full h-full'>
-          <div className='w-full'>
-            <span className='flex bg-neutral-800 py-16 border-t border-b border-iron border-opacity-60'>
-              <h2 className='text-iron text-5xl px-30 font-semibold'>Colors</h2>
-            </span>
-            <div className='flex w-full px-30 py-16 flex-row justify-evenly'>
-              <ColorDisplay
-                colorClassName='bg-torch-red'
-                colorName='Torch Red'
-                colorHex='#FF014F'
-              />
-              <ColorDisplay
-                colorClassName='bg-iron'
-                colorName='Iron'
-                colorHex='#DCDEDD'
-              />
-              <ColorDisplay
-                colorClassName='bg-alabaster'
-                colorName='Alabaster'
-                colorHex='#F9F9F9'
-              />
-              <ColorDisplay
-                colorClassName='bg-shark'
-                colorName='Shark'
-                colorHex='#212428'
-              />
-              <ColorDisplay
-                colorClassName='bg-neutral-800'
-                colorName='Neutral'
-                colorHex='#262626'
-                circleText='800'
-              />
-            </div>
-          </div>
-          <div className='w-full'>
-            <span className='flex bg-neutral-800 py-16 border-t border-b border-iron border-opacity-60'>
-              <h2 className='text-iron text-5xl px-30 font-semibold'>
-                Animations
-              </h2>
-            </span>
-          </div>
+        <div className='w-full h-full'>
+          <ColorSection />
+          <AnimationSection />
           <div className='w-full'>
             <span className='flex bg-neutral-800 py-16 border-t border-b border-iron border-opacity-60'>
               <h2 className='text-iron text-5xl px-30 font-semibold'>Link</h2>
             </span>
           </div>
-        </section>
+        </div>
       </div>
     </>
   );
---
+++ src/components/component-header.tsx
@@ -0,0 +1,5 @@
+export const ComponentHeader = ({ title }: { title: string }) => (
+  <span className='flex bg-neutral-800 py-16 border-t border-b border-iron border-opacity-60'>
+    <h2 className='text-iron text-5xl px-30 font-semibold'>{title}</h2>
+  </span>
+);
