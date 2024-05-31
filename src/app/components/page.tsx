d60b815a83204536d54b9e2aade36823fd34467d
Abraham Oh <abraham.oh@thebankoflondon.com>
2024-05-31 10:33:49 -0400

    add links section and responsiveness

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,10 +1,11 @@
-'use client';
+"use client";
 
-import { HorizontalNavBar } from '@/components/horizontal-nav-bar';
-import { MobileNavBar } from '@/components/mobile-nav-bar';
-import { AnimationSection } from '@/components/sections/animation-section';
-import { ColorSection } from '@/components/sections/color-section';
-import { useRef } from 'react';
+import { HorizontalNavBar } from "@/components/horizontal-nav-bar";
+import { MobileNavBar } from "@/components/mobile-nav-bar";
+import { AnimationSection } from "@/components/sections/animation-section";
+import { ColorSection } from "@/components/sections/color-section";
+import { LinkSection } from "@/components/sections/link-section";
+import { useRef } from "react";
 
 const ComponentsPage = () => {
   const experienceRef = useRef<HTMLElement>(null);
@@ -22,27 +23,23 @@ const ComponentsPage = () => {
         experienceRef={experienceRef}
         portfolioRef={portfolioRef}
         contactRef={contactRef}
-        // isHomePage={false}
+        isHomePage={false}
       />
-      <div className=''>
-        <section className='w-full h-screen px-30'>
-          <div className='flex w-full h-full flex-col justify-center'>
-            <h1 className='text-torch-red text-7xl font-semibold'>
+      <div className="">
+        <section className="w-full h-screen px-5 768:px-30">
+          <div className="flex w-full h-full flex-col justify-center">
+            <h1 className="text-torch-red text-7xl font-semibold">
               Design System
             </h1>
-            <h2 className='text-iron text-lg py-4 opacity-70'>
+            <h2 className="text-iron text-lg py-4 opacity-70">
               These are the components created to build this website
             </h2>
           </div>
         </section>
-        <div className='w-full h-full'>
+        <div className="w-full h-full">
           <ColorSection />
           <AnimationSection />
-          <div className='w-full'>
-            <span className='flex bg-neutral-800 py-16 border-t border-b border-iron border-opacity-60'>
-              <h2 className='text-iron text-5xl px-30 font-semibold'>Link</h2>
-            </span>
-          </div>
+          <LinkSection />
         </div>
       </div>
     </>
--- src/components/sections/color-section.tsx
+++ src/components/sections/color-section.tsx
@@ -1,35 +1,35 @@
-import { ColorDisplay } from '../color-display';
-import { ComponentHeader } from '../component-header';
+import { ColorDisplay } from "../color-display";
+import { ComponentHeader } from "../component-header";
 
 export const ColorSection = () => (
-  <section className='w-full'>
-    <ComponentHeader title='Colors' />
-    <div className='flex w-full px-30 py-16 flex-row justify-evenly'>
+  <section className="w-full">
+    <ComponentHeader title="Colors" />
+    <div className="flex w-full flex-wrap px-5 768:px-30 py-16 flex-row gap-4 768:gap-0 768:justify-evenly">
       <ColorDisplay
-        colorClassName='bg-torch-red'
-        colorName='Torch Red'
-        colorHex='#FF014F'
+        colorClassName="bg-torch-red"
+        colorName="Torch Red"
+        colorHex="#FF014F"
       />
       <ColorDisplay
-        colorClassName='bg-iron'
-        colorName='Iron'
-        colorHex='#DCDEDD'
+        colorClassName="bg-iron"
+        colorName="Iron"
+        colorHex="#DCDEDD"
       />
       <ColorDisplay
-        colorClassName='bg-alabaster'
-        colorName='Alabaster'
-        colorHex='#F9F9F9'
+        colorClassName="bg-alabaster"
+        colorName="Alabaster"
+        colorHex="#F9F9F9"
       />
       <ColorDisplay
-        colorClassName='bg-shark'
-        colorName='Shark'
-        colorHex='#212428'
+        colorClassName="bg-shark"
+        colorName="Shark"
+        colorHex="#212428"
       />
       <ColorDisplay
-        colorClassName='bg-neutral-800'
-        colorName='Neutral'
-        colorHex='#262626'
-        circleText='800'
+        colorClassName="bg-neutral-800"
+        colorName="Neutral"
+        colorHex="#262626"
+        circleText="800"
       />
     </div>
   </section>
