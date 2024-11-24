cead778a06aafb517756f405f49393af0920862c
Abraham Oh <abeoh1@gmail.com>
2024-11-24 17:44:07 -0500

    add rounded edges to portfolio cards

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Console from '/public/console.svg';
+import Link from "next/link";
+import Console from "/public/console.svg";
 
 interface ProjectProps {
   href: string;
@@ -10,18 +10,18 @@ interface ProjectProps {
 export const Project = ({ href, title, description }: ProjectProps) => (
   <Link
     href={href}
-    className='flex group w-full shadow-md bg-gradient-to-br from-black to-shark duration-500 hover:-translate-y-4 relative'
-    target='_blank'
+    className="flex group w-full shadow-md bg-gradient-to-br from-black to-shark duration-500 hover:-translate-y-4 relative rounded-2xl"
+    target="_blank"
   >
-    <span className='flex flex-col w-full m-12'>
-      <span className='flex w-12 h-12 justify-start mb-10'>
-        <Console className='flex h-full text-torch-red' />
+    <span className="flex flex-col w-full m-12">
+      <span className="flex w-12 h-12 justify-start mb-10">
+        <Console className="flex h-full text-torch-red" />
       </span>
       <span>
-        <h3 className='text-2xl text-alabaster group-hover:text-torch-red'>
+        <h3 className="text-2xl text-alabaster group-hover:text-torch-red">
           {title}
         </h3>
-        <p className='text-iron mt-4'>{description}</p>
+        <p className="text-iron mt-4">{description}</p>
       </span>
     </span>
   </Link>
