413f4bdfca4f5d7f5785873229dbbffb812ab768
Abraham Oh <abeoh1@gmail.com>
2024-11-24 17:42:10 -0500

    fix email aligment

--- src/components/email.tsx
+++ src/components/email.tsx
@@ -1,6 +1,11 @@
 export const Email = () => (
-  <div className='fixed hidden 768:flex bottom-0 right-0 flex flex-col w-20 items-center opacity-70'>
-    <p className='rotate-90 text-iron pr-40 tracking-wide'>abeoh1@gmail.com</p>
-    <div className='bg-iron/70 w-0.5 h-20' />
+  <div className="fixed hidden 768:flex bottom-0 right-0 flex flex-col w-20 items-center opacity-70">
+    <p
+      className="text-iron pr-40 tracking-wide"
+      style={{ transform: "rotate(90deg) translateY(-10%)" }}
+    >
+      abeoh1@gmail.com
+    </p>
+    <div className="bg-iron/70 w-0.5 h-20" />
   </div>
 );
