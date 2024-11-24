0d2cbe5dc86ad33dd4685b4c20da80db390509b0
Abraham Oh <abeoh1@gmail.com>
2024-11-24 17:14:50 -0500

    update tag visual bug and experience

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,5 +1,5 @@
 export const Tag = ({ textContent }: { textContent: string }) => (
-  <div className='flex relative py-2 pl-3 pr-4 mt-0.5 text-lg bg-black h-fit leading-none text-alabaster after:content-[""] after:absolute after:top-0 after:right-0 after:border-t-[17px] after:border-t-shark after:border-b-[17px] after:border-b-shark after:border-l-[10px] after:border-l-black before:w-0'>
+  <div className='flex relative py-2 pl-3 pr-4 mt-0.5 text-lg bg-black h-fit leading-none text-alabaster after:content-[""] after:absolute after:top-0 after:right-0 after:border-t-[17px] after:border-t-shark after:border-b-[18px] after:border-b-shark after:border-l-[10px] after:border-l-black before:w-0'>
     {textContent}
   </div>
 );
